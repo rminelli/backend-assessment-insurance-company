6f48e785ea49ac3c5fd567ee16ace2f9b9433524
Roberto Minelli <rminelli@gmail.com>
2019-09-29 00:27:46 -0300

    added tests

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -10,7 +10,7 @@ describe('Test the root path', () => {
     });
 });
 
-describe('Test the generate client token', () => {
+describe('Test the api path', () => {
   test('It should response the GET method', (done) => {
       request(app).post('/api/').then((response) => {
           expect(response.statusCode).toBe(404);
@@ -18,3 +18,24 @@ describe('Test the generate client token', () => {
       });
   });
 });
+
+
+describe('Test the generate client token', () => {
+test('Get a valid token', async(done) => {
+  const service = {
+      name: "Jacquelyna",
+      email: "jacquelynblankenship@quotezart.com"
+  };
+  try {
+      const count = await Service.count();
+      await request(app).post('/api/').send(service)
+      const newCount = await Service.count()
+      expect(response.statusCode).toBe(200);
+      done()
+  } catch (err) {
+      // write test for failure here
+      console.log(`Error ${err}`)
+      done()
+  }
+})
+})
