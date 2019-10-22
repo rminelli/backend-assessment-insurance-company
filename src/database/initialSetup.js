e6e0926d05ba0e8f1e181db9b76f83ad71612993
Roberto Minelli <rminelli@gmail.com>
2019-10-22 15:51:30 -0300

    removed unused dependencies

--- src/database/initialSetup.js
+++ src/database/initialSetup.js
@@ -1,5 +1,4 @@
 const getData = require('./getData')
-const createDatabase = require('./createDatabase')
 const createDB = require('./createDatabase')
 const insertClientsData = require('./insertClientsData')
 const insertPoliciesData = require('./insertPoliciesData')
@@ -8,16 +7,16 @@ const clientsUrl = "http://www.mocky.io/v2/5808862710000087232b75ac"
 const policiesUrl = "http://www.mocky.io/v2/580891a4100000e8242b75c5"
 
 
-createDB(database)     
+createDB(database)
     .then(response => insertData())
     .catch(err => console.log(`Erro : ${err}`))
 
 function insertData() {
-getData(clientsUrl)
-     .then(response => insertClientsData(response.data.clients))
-     .then(response => getData(policiesUrl)
-     .then(response => insertPoliciesData(response.data.policies)))
-     .catch(err => console.log(`Erro : ${err}`))
+    getData(clientsUrl)
+        .then(response => insertClientsData(response.data.clients))
+        .then(response => getData(policiesUrl)
+            .then(response => insertPoliciesData(response.data.policies)))
+        .catch(err => console.log(`Erro : ${err}`))
 }
 
 
